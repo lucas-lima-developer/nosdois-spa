4e84f3e31836901af4d359ed477c70665d994000
Lucas Lima <lucasvinilima15@gmail.com>
2025-05-22 20:29:14 -0300

    feat: add routing with react-router and create Dashboard and Login pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard.tsx'
+import Login from './pages/Login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
